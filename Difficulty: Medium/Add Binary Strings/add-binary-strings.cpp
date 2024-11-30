42cd0b5ec34f7e808c3294c918dad8a080c3a2cd
MohdZibran <2k22.it.32328@gmail.com>
2024-11-30 22:40:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add Binary Strings/add-binary-strings.cpp
+++ Difficulty: Medium/Add Binary Strings/add-binary-strings.cpp
@@ -8,32 +8,26 @@ using namespace std;
 class Solution {
   public:
     string addBinary(string& s1, string& s2) {
-        string ans = "";  // To store the binary sum
-        int i = s1.size() - 1;  // Pointer for `s1`
-        int j = s2.size() - 1;  // Pointer for `s2`
-        int carry = 0;  // Initialize carry
-
-        // Process both strings until all bits are covered
+        string ans = "";  
+        int i = s1.size() - 1; 
+        int j = s2.size() - 1;  
+        int carry = 0;  
         while (i >= 0 || j >= 0 || carry > 0) {
-            int count1s = carry;  // Start with carry
+            int count1 = carry;  
+            if (i >= 0 && s1[i] == '1') count1++;
 
-            // Add 1 if the current bit of `s1` is '1'
-            if (i >= 0 && s1[i] == '1') count1s++;
+            if (j >= 0 && s2[j] == '1') count1++;
 
-            // Add 1 if the current bit of `s2` is '1'
-            if (j >= 0 && s2[j] == '1') count1s++;
-
-            // Append the result bit to `ans`
-            ans += (count1s % 2 == 0 ? '0' : '1');
-
-            // Update carry
-            carry = count1s / 2;
-
-            // Move pointers to the next bit
+            if (count1 % 2 == 0) {
+                ans+='0';
+                }
+            else {
+                    ans+='1';
+                }
+            carry = count1 / 2;
             i--;
             j--;
         }
-        // Reverse the result string to correct its order
         reverse(ans.begin(), ans.end());
         // To avoid leading zeros
         i=0;
