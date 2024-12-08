c05c940766194c3bbf99c941da6142c0f568bf0d
MohdZibran <2k22.it.32328@gmail.com>
2024-12-09 01:09:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.cpp
@@ -6,46 +6,28 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
-        // Code here
-    //     vector<vector<int>>ans;
-    //      vector<vector<int>>index;
-    //     //sorting on the basis of start
-    //     sort(arr.begin(),arr.end());
-    //     int start=arr[0][0];
-    //     int end=arr[0][1];
-    //     int n=arr.size();
-    //     int j=1;
-    //     for(int i=1;i<n;i++){
-    //         if(arr[i][0]<=arr[i-1][1]){
-    //             index.push_back({i-1},{i});
-    //             end=arr[i][0];
-    //             cnt++;
-    //         }
-    //     }
-    //     if(cnt==n-1){
-    //         return {{start,end}};
-    //     }
-    //     for(int i=0;i<index.size();i++){
-    //         int first=[i][0];
-    //         int second=[i][1];
-    //         vec.erase(vec.begin() + i);
-
-    //     }
-    //     return arr;
-    // }
-    vector<vector<int>>ans;
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<arr.size();i++){
-            if(ans.empty() || ans.back()[1]<arr[i][0]){
-                ans.push_back(arr[i]);
-            }
-            else{
-                ans.back()[1]=max(ans.back()[1],arr[i][1]);
-            }
-        }
-        return ans;
+   vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+    if (arr.empty()) {
+        return {};
     }
+    sort(arr.begin(), arr.end());
+    vector<vector<int>> ans;
+    int start = arr[0][0];
+    int end = arr[0][1];
+
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i][0] <= end) {
+            end = max(end, arr[i][1]);
+        } else {
+            ans.push_back({start, end});
+            start = arr[i][0];
+            end = arr[i][1];
+        }
+    }
+    ans.push_back({start, end});
+
+    return ans;
+}
 };
 
 //{ Driver Code Starts.
